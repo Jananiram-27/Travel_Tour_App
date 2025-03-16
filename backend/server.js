d0e443f0a5e818648f7ede1d4f66cc6e5f39f279
janani <chravija@gmail.com>
2025-03-16 11:41:59 +0530

    changing

--- backend/server.js
+++ backend/server.js
@@ -67,6 +67,9 @@ app.post("/login", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
